0319ff80720264b021ae210f52829d268dcf7307
Michael Herman <hermanmu@gmail.com>
2021-06-04 16:27:14 -0500

    revision

--- quote/models.py
+++ quote/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Quote(models.Model):
+    name = models.CharField(max_length=250)
+    quote = models.TextField(max_length=1000)
 
-class QuoteTaking(models.Model):
-  name = models.CharField(max_length=250)
-  quote = models.TextField(max_length=1000)
-
-  def __str__(self):
-    return self.quote
+    def __str__(self):
+        return self.quote
