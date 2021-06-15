a608e68a7dfffcb96898bbf2ccf3d6bae0e2562e
samuel <efosonaname@gmail.com>
2021-06-15 14:15:38 +0100

    init

--- quote/views.py
+++ quote/views.py
@@ -1,16 +1,17 @@
-from .models import Quote
-from django.views.generic import ListView
-from django.db.models import Q
 from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.db.models import Q
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView
+from django.views.decorators.cache import cache_page
 
-# Create your views here.
+from .models import Quote
 
 
+@method_decorator(cache_page(60 * 5), name="dispatch")
 class QuoteList(ListView):
     model = Quote
     context_object_name = "quotes"
     template_name = "quote.html"
-    paginate_by = 10
 
 
 """ Q objects """
